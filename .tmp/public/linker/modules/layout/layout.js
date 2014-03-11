aed33a76036c60a8c283839fd1558f094048696f
Kehinde Alao <kehinde.alao@hotmail.com>
2014-03-11 17:01:47 +0100

    again2

--- .tmp/public/linker/modules/layout/layout.js
+++ .tmp/public/linker/modules/layout/layout.js
@@ -4,6 +4,10 @@ angular.module('quantum.layout',['ui.router'])
     .controller('HeaderController',['$scope',function($scope){
     $scope.me=this;
 }])
-    .controller('FooterController',['$scope',function($scope){
+    .controller('FooterController',['$scope','$window',function($scope,$window){
         $scope.me=this;
+        $scope.toTop=function(){
+            console.log('Scroll to top clicked');
+            $window.scrollTo(0);
+        }
     }]);
